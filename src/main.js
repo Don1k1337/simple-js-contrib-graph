35c23ca713d2efc6c5605f5ce484ebf22e3b0b2c
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-07-03 15:36:32 +0600

    feature: data fetching, graph UI implement.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-// Main script file
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://dpg.gg/test/calendar.json');
+        const data = await response.json();
+
+        const squaresUl = document.querySelector('.squares');
+
+        for (const date in data) {
+            const level = data[date];
+            const li = document.createElement('li');
+            li.setAttribute('data-level', level);
+            squaresUl.appendChild(li);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+(async () => {
+    await fetchData();
+})();
